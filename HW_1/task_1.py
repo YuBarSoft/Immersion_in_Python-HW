24afdc98cdbf8adc7895420cd7165e0ef48f40b2
YuBarSoft <monamurs@gmail.com>
2023-06-15 17:06:21 +1000

    All Task's solved.

--- HW_1/task_1.py
+++ HW_1/task_1.py
@@ -1,1 +1,17 @@
-# тут решение 1 задачи
+# 1. Пишем таблицу умножения из семинара.
+
+min_digit = 2
+max_digit = 10
+tower = 4
+
+for i in range(min_digit, max_digit, tower):
+    print()
+    for j in range(min_digit, max_digit + 1):
+        for t in range(i, i + tower):
+            if j == min_digit and t == i + tower - 1:
+                print(f'{t:>} x {j:>2} = {t * j:>2}\n', end='')
+            elif t == i + tower - 1:
+                print(f'{t:>} x {j:>2} = {t * j:>2}\n', end='')
+            else:
+                print(f'{t:>} x {j:>2} = {t * j:>2}\t\t', end='')
+
---
+++ HW_1/task_4.py
@@ -0,0 +1,33 @@
+# 4. Программа загадывает число от 0 до 1000. Необходимо угадать число за 10 попыток. Программа должна подсказывать
+# «больше» или «меньше» после каждой попытки. Для генерации случайного числа используйте код:
+# from random import
+# num = randint(LOWER_LIMIT, UPPER_LIMIT)
+
+from random import randint
+
+LOWER_LIMIT = 0
+UPPER_LIMIT = 1000
+counter_max = 10
+counter = 0
+num = randint(LOWER_LIMIT, UPPER_LIMIT+1)
+if_right = True
+
+print(f'Для тестирования: компьютер загадал число: {num}\n')
+
+for i in range(1, counter_max+1):
+    counter += 1
+    number = int(input(f'Попытка №{counter} из {counter_max}. Введите число от 0 до 1000: '))
+    if number > num:
+        print('\tВаше число БОЛЬШЕ загаданного компьютером\n')
+        if_right = False
+    elif number < num:
+        print('\tВаше число МЕНЬШЕ загаданного компьютером\n')
+        if_right = False
+    else:
+        print(f'''\tВы угадали загаданное компьютером число c {counter} попытки!
+\tПриглашаем Вас на тв-шоу "Битва экстрасенсов"!''')
+        if_right = True
+        break
+
+if not if_right:
+    print('К сожалению, Вы не смогли угадать число.')
