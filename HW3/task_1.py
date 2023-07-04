34d80672ef7c942bd819e6688db0fb7c5c859f76
Iurii Baranov <111112727+YuBarSoft@users.noreply.github.com>
2023-07-04 06:12:39 +0300

    task_1.py: work on bugs

--- HW3/task_1.py
+++ HW3/task_1.py
@@ -8,19 +8,24 @@
 
 data = {'Алексей': ('палатка', 'котелок', 'мангал', 'топор', 'веревка'),
         'Константин': ('гитара', 'алкоголь', 'продукты', 'палатка', 'туалетные принадлежности', 'нож'),
-        'Анна': ('туалетные принадлежности', 'столовые приборы', 'скатерть', 'походный стол со стульями', 'кастрюля', 'сковорода', 'палатка')}
+        'Анна': ('туалетные принадлежности', 'столовые приборы', 'скатерть', 'походный стол со стульями', 'кастрюля',
+                 'сковорода', 'палатка')}
 
-print('\n1. Какие вещи взяли все три друга?')
-all_frends_values = set.intersection(*map(set, data.values()))
-print(f"\t{', '.join(all_frends_values)}")
+print('1. Какие вещи взяли все три друга?')
+# all_frends_values = set.intersection(*map(set, data.values()))  # это вариант про вещи, которые есть у всех друзей
+all_items = set()
+for items in data.values():
+    all_items.update(items)
+all_items = list(all_items)
+for item in all_items:
+    print(f'\t{item}')
 
+print('-------------------------\n2. Какие вещи уникальны (есть только у одного друга)? Укажите имя этого друга.')
 lst = []
 for frend in data:
     lst.extend(list(data[frend]))
 no_item = set()
 
-print('''-------------------------
-2. Какие вещи уникальны (есть только у одного друга)? Укажите имя этого друга.''')
 for frend, value in data.items():
     for i in range(len(value)):
         if lst.count(value[i]) == 1:
@@ -28,10 +33,8 @@ for frend, value in data.items():
         if lst.count(value[i]) == len(data) - 1:
             no_item.add(value[i])
 
-print('''-------------------------
-3. Какие вещи есть у всех друзей, кроме одного? Укажите имя того, у кого данная вещь отсутствует.''')
+print('-------------------------\n3. Какие вещи есть у всех друзей, кроме одного? Укажите имя того, у кого данная вещь отсутствует.')
 for frend, value in data.items():
     for i in no_item:
         if i not in data[frend]:
             print(f'\t{i} - {frend}')
-
