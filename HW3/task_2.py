0f23068bbb5867a2157a3bbeb993a22b98c0c894
Iurii Baranov <111112727+YuBarSoft@users.noreply.github.com>
2023-07-04 05:07:41 +0300

    task_2.py: work on bugs

--- HW3/task_2.py
+++ HW3/task_2.py
@@ -3,18 +3,20 @@
 # В результирующем списке не должно быть дубликатов.
 
 import random
+from collections import Counter
 
 numbers = list(range(1, 101))  # создаем список чисел от 1 до 100
 lst = []
 for i in range(10):
     num = random.choice(numbers)  # выбираем случайное число из списка
-    count = random.randint(2, 3)  # генерируем случайное количество повторений (2 или 3)
+    count = random.randint(1, 3)  # генерируем случайное количество повторений (1 или 3)
     lst.extend([num] * count)  # добавляем число в список lst нужное количество раз
-print(f'Дан список повторяющихся элементов:\n{lst}')
+print(f'Дан список элементов:\n{lst}')
 
 result = []
 for number in lst:
-    if number not in result:
+    if number not in result and lst.count(number) > 1:
         result.append(number)
+
 print(f'Cписок с дублирующимися элементами:\n{result}')
 
