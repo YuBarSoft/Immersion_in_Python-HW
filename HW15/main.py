0050dd052d57b54769b2eb500c4e9a50ca2b5719
Iurii Baranov <111112727+YuBarSoft@users.noreply.github.com>
2023-08-07 04:07:31 +0300

    Update main.py

--- HW15/main.py
+++ HW15/main.py
@@ -10,7 +10,6 @@
 """
 
 import argparse
-import logging
 from logger import setup_logger
 from directory_info import collect_directory_info
 
@@ -20,19 +19,18 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     logger = setup_logger('logfile.txt')
-    
+
     try:
         result = collect_directory_info(args.path)
-
         if result:
             for item in result:
                 logger.info(f"Name: {item.name},"
-                             f" Extension: {item.extension},"
-                             f" Is Directory: {item.is_directory},"
-                             f" Parent Directory: {item.parent_directory}")
+                            f" Extension: {item.extension},"
+                            f" Is Directory: {item.is_directory},"
+                            f" Parent Directory: {item.parent_directory}")
             print("Информация о директории записана в файл logfile.txt.")
         else:
             print("Произошла ошибка. Проверьте путь до директории и данные в лог-файле.")
 
     except Exception as e:
-        logging.error(f'Произошла ошибка: {str(e)}')
+        logger.error(f'Произошла ошибка: {str(e)}')
