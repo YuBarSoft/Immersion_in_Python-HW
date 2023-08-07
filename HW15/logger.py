aac0686e836a0c5fdb05ff48a759240b0882b278
Iurii Baranov <111112727+YuBarSoft@users.noreply.github.com>
2023-08-07 04:08:12 +0300

    Update logger.py

--- HW15/logger.py
+++ HW15/logger.py
@@ -2,4 +2,5 @@ import logging
 
 def setup_logger(logfile):
     logging.basicConfig(filename=logfile, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
     return logging.getLogger()
